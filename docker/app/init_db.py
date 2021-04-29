b34223d36eb4a49e0bbae3c2c83055d45eb9f525
Matt Austin <matt.austin@contrastsecurity.com>
2021-04-29 15:18:36 -0700

    fixed cat image

--- docker/app/init_db.py
+++ docker/app/init_db.py
@@ -8,7 +8,7 @@ with open('schema.sql') as f:
 
 cur = connection.cursor()
 
-cur.execute("INSERT INTO notes (content) VALUES (?)", ("Hello Captain, <br><br>Welcome to the Captain's log. You can add your notes as you continue on your journey. <br><br><img src='https://upload.wikimedia.org/wikipedia/commons/c/c6/IdeaLab_space_cat.svg'><br><br>../SpaceCat",))
+cur.execute("INSERT INTO notes (content) VALUES (?)", ("Hello Captain, <br><br>Welcome to the Captain's log. You can add your notes as you continue on your journey. <br><br><img src='https://maustin.net/img/contrast_cat.png'><br><br>../SpaceCat",))
 
 connection.commit()
 connection.close()
